766f35224d929416f13e2b2ea7ee6674394b6581
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-07-17 22:15:29 +0200

    jok

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,8 @@
 module.exports.config = {
-    uri: 'mongodb+srv://admin:anime@cluster0-e7lsm.mongodb.net/test?retryWrites=true',
+    uri:
+//"mongodb+srv://ayoub:7LchjMQ9ENeSbwcw@cluster0.fgqdy.mongodb.net/MyCookBook?retryWrites=true&w=majority",
+ 'mongodb+srv://admin1:anime@cluster0.fgqdy.mongodb.net/redis?retryWrites=true',
+  //'mongodb+srv://admin:anime@cluster0-e7lsm.mongodb.net/test?retryWrites=true',
     options: {
         dbName: 'reddit-app',
         useNewUrlParser: true
